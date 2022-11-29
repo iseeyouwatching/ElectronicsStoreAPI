881038a9fbaeb1fb1089699f191e301d511bd752
iseeyouwatching <leonidshibanovv@gmail.com>
2022-11-29 11:25:58 +0700

    Initial commit

---
+++ src/test/java/ru/javaops/bootjava/electronicsstore/ElectronicsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.bootjava.electronicsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
