879cb6417b6ff19c5c1b427f59765b2558da1248
iseeyouwatching <leonidshibanovv@gmail.com>
2022-11-30 14:45:42 +0700

    Added end point implementations
    There are the following end point implementations:
    - laptop addition
    - laptop update
    - laptop delete
    - viewing all existing laptops
    - viewing a laptop by ID

---
+++ src/main/java/ru/javaops/bootjava/electronicsstore/repository/LaptopRepository.java
@@ -0,0 +1,11 @@
+package ru.javaops.bootjava.electronicsstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.javaops.bootjava.electronicsstore.entity.LaptopEntity;
+
+import java.util.UUID;
+
+@Repository
+public interface LaptopRepository  extends JpaRepository<LaptopEntity, UUID> {
+}
